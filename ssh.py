d05991eb4de6784a0c321ab0d483efca3145ae26
Vikash Kumar <vikashkumar.cse19@chitkarauniversity.edu.in>
2022-02-18 01:04:20 -0500

    er

--- ssh.py
+++ ssh.py
@@ -1,10 +1,1 @@
 #!/bin/python
-
-import argparse
-
-parser = argparse.ArgumentParser(description='Fetch comments and anchor tags from a website')
-parser.add_argument('-u', '--urll', type=str, help='URL to scrape.')
-
-arg = parser.parse_args()
-
-print(arg.urll)
