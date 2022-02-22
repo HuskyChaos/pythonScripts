83b141bbc3e342bd66c68d7af70a2c433585bba3
Vikash Kumar <vikashkumar.cse19@chitkarauniversity.edu.in>
2022-02-21 23:51:51 -0500

    upgrade 1

--- ssh.py
+++ ssh.py
@@ -1,1 +1,18 @@
-#!/bin/python
+#!/bin/python3
+import subprocess
+
+subprocess.run(["sudo", "su"])
+
+sub = subprocess.run(["sudo","service", "ssh", "status"], capture_output=True)
+sub = sub.stdout.decode()
+
+if "dead" in sub:
+    subprocess.run(["sudo","systemctl","enable", "--now", "ssh"])
+
+else:
+    print("SSH Active")
+
+rootlist = subprocess.run(["ls", "-la","/root/"], capture_output=True)
+rootlist = rootlist.stdout.decode()
+if ".ssh" not in rootlist:
+    subprocess.run(["", ""])
