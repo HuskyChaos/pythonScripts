d89562b3e822617612a6c3bcff8b98eea6b455fa
vikashkr99 <vikashkrdgr8.2505@gmail.com>
2022-06-17 04:36:20 -0400

    Done

--- project1/test.py
+++ project1/test.py
@@ -1,5 +1,7 @@
 #!/bin/python3
 import time
+from numpy import empty
+import requests
 from typing import get_type_hints
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.service import Service
@@ -9,27 +11,44 @@ profile_path = r'/home/kali/.mozilla/firefox/06au6nqy.default'
 options=Options()
 options.set_preference('profile', profile_path)
 service = Service(r'./geckodriver')
+url = "https://nbl.one/listings"
 
-driver = Firefox(service=service, options=options)
-
-driver.get("https://nbl.one/listings")
-
-print("Python Waiting for web page to finish loading...")
-
-for i in range(10,0,-1):
-    print("{}....".format(i))
-    time.sleep(1)
-
-print()
-classes = driver.find_elements_by_class_name('gig_cards')
-
-for c in classes:
-    if c.text != '':
-        temp = c.text.split('\n')
-        print(temp[0])
-        for i in temp:
-            if 'Starts' in i:
-                print(i)
-        print ('--------------------------------------------------')
-
-driver.quit()
+r = requests.get(url)
+if r.status_code >= 400:
+    exit(r.reason)
+else:
+    try:
+        driver = Firefox(service=service, options=options)
+        driver.get(url)
+        print("Python Waiting for web page to finish loading...")
+        for i in range(10,0,-1):
+            print("{}....".format(i))
+            time.sleep(1)
+        print()
+        count = 0
+        classes = driver.find_elements_by_class_name('gig_cards')
+        for i in classes:
+            if i.text != '':
+                count+=1
+        for i in range(count):
+            classes = driver.find_elements_by_class_name('gig_cards')
+            count2 = 0
+            for c in classes:
+                if c.text != '':
+                    if count2 == i:
+                        temp = c.text.split('\n')
+                        print(temp[0])
+                        for i in temp:
+                            if 'Starts' in i:
+                                print(i)
+                        c.click()
+                        time.sleep(2)
+                        print(driver.current_url)
+                        driver.execute_script("window.history.go(-1)")
+                        time.sleep(2)
+                        print ('--------------------------------------------------')
+                        break
+                    count2+=1
+        driver.quit()
+    except Exception as e:
+        print(e)
