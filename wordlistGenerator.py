6052f4dffa40b6971231214fd1d0d3af21392b15
Vikash Kumar <vikashkrdgr8.2505@gmail.com>
2022-08-31 00:28:43 +0530

    specify char length available

--- wordlistGenerator.py
+++ wordlistGenerator.py
@@ -1,45 +1,94 @@
-def generateTextFile(uList):
-    myWordList = open("./wordList.txt", "w")
-    for i in uList:
+def generateTextFile(wordList, fileName):
+    myWordList = open(f"./{fileName}.txt", "w")
+    for i in wordList:
         myWordList.write(f'{i}\n')
     myWordList.close()
+    
 
-def uniqueList(tempList):
-    uList = list()
-    for word in tempList:
-        if word not in uList:
-            uList.append(word)
-    generateTextFile(uList)
-
-def main(charSet, mode):
-    baseList = charSet
-    newList = baseList
+def option1 (charSet):
+    newList = charSet
     tempList = list()
+    mixedList = list()
     num=0
 
-    if mode == 0:
-        myWordList = open("./wordList.txt", "w")
-        while(num<len(baseList)):
-            for i in newList:
-                myWordList.write(f'{i}\n')
-                for j in baseList:
-                    tempList.append(i+j)
-            newList=tempList
-            tempList=list()
-            num+=1
-        myWordList.close()        
+    while(num<len(charSet)):
+        for i in newList:
+            mixedList.append(i)
+            for j in charSet:
+                tempList.append(i+j)
+        newList=tempList
+        tempList=list()
+        num+=1
 
-    else:    
-        mixedList = list()
-        while(num<len(baseList)):
-            for i in newList:
-                mixedList.append(i)
-                for j in baseList:
-                    tempList.append(i+j)
-            newList=tempList
-            tempList=list()
-            num+=1
-        uniqueList(mixedList)
+    wordList = list()
+    for word in mixedList:
+        if word not in wordList:
+            wordList.append(word)
+    generateTextFile(wordList, "custom_char_wl")
+
+def option2 (charLen, charSet):
+    wordList = list()
+    word=""
+    tempWord = list()
+    for i in range(charLen):
+        tempWord.append(0)
+        word+=charSet[0]
+    wordList.append(word)
+    while True:
+        if tempWord[charLen-1] < 94:
+            tempWord[charLen-1]+=1
+        elif sum(tempWord) == 94*charLen:
+            break
+        else:
+            tempWord[charLen-1] = 0
+            for i in range(charLen-2, -1, -1):
+                if tempWord[i] < 94:
+                    tempWord[i]+=1
+                    break
+                else:
+                    tempWord[i] = 0
+        word = "" 
+        for j in tempWord:
+            word+=charSet[j]
+        wordList.append(word)
+    generateTextFile(wordList, "word_length_wl")
+
+def option3 (charSet):
+    newList = charSet
+    tempList = list()
+    num=0
+    myWordList = open("./max_limit_wl.txt", "w")
+    while(num<len(charSet)):
+        for i in newList:
+            myWordList.write(f'{i}\n')
+            for j in charSet:
+                tempList.append(i+j)
+        newList=tempList
+        tempList=list()
+        num+=1
+    myWordList.close()
+
+def main ():
+    charSet = list("""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_+=-,./';][<>?":|}{\\ """)
+    while True:
+        option = input("""1. Enter custom characters\n2. Enter a word length.\n3. Use pre-existing character set\n   (word length starts from 1 ends at 97)\n   (Choose wisely)\n4. Enter q to Exit!\n\n Option number: """)
+        if option == "1":
+            charSet = list(input("Enter characters (space will be considered a character): \n"))
+            option1(charSet)
+
+        elif option == "2":
+            charLen = int(input("Enter desired word length: "))
+            option2(charLen, charSet)
+        
+        elif option == "3":
+            option3(charSet)
+
+        elif option == "q":
+            quit(f"\n\t====Thanks for using WORDLIST GENERATOR====\n")
+        
+        else:
+            print("Choose a valid option!")
+
 
 if __name__ == '__main__':
     print("""   
@@ -50,17 +99,9 @@ if __name__ == '__main__':
              █▄█ ██▄ █░▀█ ██▄ █▀▄ █▀█ ░█░ █▄█ █▀▄         
     """)
     print("Before running this script, make sure that you have write permission.")
-    print("Enter q if you wish to exit!")
     print()
-    while True:
-        option = input("Enter 1 to use your own letters or press Enter to use pre-existing wordlist: ")
-        if option == "1":
-            charSet = list(input("Enter characters without space:\n"))
-            mode = 1
-        elif option == "q":
-            quit(f"             ====Thanks for using WORDLIST GENERATOR====")
-        else:
-            charSet = list("""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_+=-,./';][<>?":|}{\\ """)
-            mode = 0
-        main(charSet, mode)
+    main()
+
+
+
     
