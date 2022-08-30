6dd9134d16c5c4a397e9fc59b1fef442cd1bbe26
Vikash Kumar <vikashkrdgr8.2505@gmail.com>
2022-08-30 20:05:14 +0530

    remove duplicates

--- wordlistGenerator.py
+++ wordlistGenerator.py
@@ -1,23 +1,47 @@
-def main(charSet):
+def generateTextFile(uList):
+    myWordList = open("./wordList.txt", "w")
+    for i in uList:
+        myWordList.write(f'{i}\n')
+    myWordList.close()
+
+def uniqueList(tempList):
+    uList = list()
+    for word in tempList:
+        if word not in uList:
+            uList.append(word)
+    generateTextFile(uList)
+
+def main(charSet, mode):
     baseList = charSet
     newList = baseList
     tempList = list()
-    myWordList = open("./wordList.txt", "w")
     num=0
-    while(num<len(baseList)):
-        for i in newList:
-            myWordList.write(f'{i}\n')
-            for j in baseList:
-                tempList.append(i+j)
-        newList=tempList
-        tempList=list()
-        num+=1
-    myWordList.close()
 
+    if mode == 0:
+        myWordList = open("./wordList.txt", "w")
+        while(num<len(baseList)):
+            for i in newList:
+                myWordList.write(f'{i}\n')
+                for j in baseList:
+                    tempList.append(i+j)
+            newList=tempList
+            tempList=list()
+            num+=1
+        myWordList.close()        
+
+    else:    
+        mixedList = list()
+        while(num<len(baseList)):
+            for i in newList:
+                mixedList.append(i)
+                for j in baseList:
+                    tempList.append(i+j)
+            newList=tempList
+            tempList=list()
+            num+=1
+        uniqueList(mixedList)
 
 if __name__ == '__main__':
-
-
     print("""   
                 █░█░█ █▀█ █▀█ █▀▄ █░░ █ █▀ ▀█▀  
                 ▀▄▀▄▀ █▄█ █▀▄ █▄▀ █▄▄ █ ▄█ ░█░  
@@ -32,9 +56,11 @@ if __name__ == '__main__':
         option = input("Enter 1 to use your own letters or press Enter to use pre-existing wordlist: ")
         if option == "1":
             charSet = list(input("Enter characters without space:\n"))
+            mode = 1
         elif option == "q":
             quit(f"             ====Thanks for using WORDLIST GENERATOR====")
         else:
             charSet = list("""abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()_+=-,./';][<>?":|}{\\ """)
-        main(charSet)
+            mode = 0
+        main(charSet, mode)
     
